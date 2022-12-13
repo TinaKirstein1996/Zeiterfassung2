96921a924e3db44d79f7babf0e75444e6eaf9a27
Tina Kirstein <tina@kirsteins.de>
2022-12-13 02:32:37 +0100

    Saving data as logged in user is now possible

--- src/main/webapp/data_management.php
+++ src/main/webapp/data_management.php
@@ -1,13 +1,14 @@
 <?php
+session_start();
 
-$p_nr = $_POST['p_nr'];
+$userid = $_SESSION['userid'];
 $date = $_POST['date'];
 $start_time = $_POST['start_time'];
 $pause_time = $_POST['pause_time'];
 $end_time = $_POST['end_time'];
 $note = $_POST['note'];
 
-if (!empty($p_nr) || !empty($date) || !empty($start_time) || !empty($end_time)) {
+if (!empty($date) || !empty($start_time) || !empty($end_time)) {
     $dbhost="rdbms.strato.de";
     $dbuser="dbu433728";
     $dbpass="projektarbeit";
@@ -18,32 +19,36 @@ if (!empty($p_nr) || !empty($date) || !empty($start_time) || !empty($end_time)) 
     if (mysqli_connect_error()){
         die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     } else {
+        
         $SELECT = "SELECT note From tracked_time Where note = ? Limit 1";
+        $SELECTp_nr = "SELECT p_nr From users Where id = ? Limit 1";
         $INSERT = "INSERT INTO tracked_time (p_nr, date, start_time, pause_time, end_time, note) VALUES (?, ?, ?, ?, ?, ?)";
         
-        $stmt = $connect->prepare($SELECT);
-        $stmt->bind_param("s", $note);
+        $stmt = $connect->prepare($SELECTp_nr);
+        $stmt->bind_param("i", $userid);
         $stmt->execute();
-        $stmt->bind_result($note);
-        $stmt->store_result();
-        $rnum = $stmt->num_rows;
+        $stmt->bind_result($p_nr);
+        $stmt->fetch();
         
-        if ($rnum==0) {
+        if ($p_nr!==0) {
             $stmt->close();
             
             $stmt=$connect->prepare($INSERT);
             $stmt->bind_param("isssss", $p_nr, $date, $start_time, $pause_time, $end_time, $note);
             $stmt->execute();
-            echo "Daten erfolgreich eingetragen!";
+            header('Location: http://zeiterfassung-wbh.de/Dashboard.html');
+            exit;
         } else {
-            echo "Diese Notiz wurde bereits verwendet";
+            echo "Diese Personalnummer ist leider nicht verfügbar";
+            header('Location: http://zeiterfassung-wbh.de/Dashboard.html');
+            exit;
         }
         $stmt->close();
         $connect->close();
     }
 } else {
     echo "Alle Felder müssen ausgefüllt werden!";
-    die;
-}
+    die;}
+
 
 ?>

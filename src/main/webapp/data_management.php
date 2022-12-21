c756e65535106f9cc93ad52eaf6fb83d541c47b6
Tina Kirstein <tina@kirsteins.de>
2022-12-21 07:30:11 +0100

    Added function to calculate worked hours with pause

--- src/main/webapp/data_management.php
+++ src/main/webapp/data_management.php
@@ -3,12 +3,14 @@ session_start();
 
 $userid = $_SESSION['userid'];
 $date = $_POST['date'];
+
 $start_time_hour = $_POST['hour_start'];
 $start_time_minute = $_POST['minute_start'];
 $pause_time_start = $_POST['hour_pause'];
 $pause_time_minute = $_POST['max_pause'];
 $end_time_hour = $_POST['hour_end'];
 $end_time_hour = $_POST['minute_end'];
+
 $note = $_POST['note'];
 
 if (!empty($date) || !empty($start_time_hour) || !empty($end_time_hour)) {
@@ -43,13 +45,15 @@ if (!empty($date) || !empty($start_time_hour) || !empty($end_time_hour)) {
             $end_time = $_POST['hour_end'].":".$_POST['minute_end'];
             
             $worked_start = $_POST['date']." ".$start_time;
+            $worked_pause = $_POST['date']." ".$pause_time;
             $worked_end = $_POST['date']." ".$end_time;
             $starttimestamp = strtotime($worked_start);
             $endtimestamp = strtotime($worked_end);
             
-            function differenceInHours($worked_start,$worked_end){
+            function differenceInHours($worked_start,$worked_end,$worked_pause, $date){
                 $starttimestamp = strtotime($worked_start);
                 $endtimestamp = strtotime($worked_end);
+                $pausetimestamp = strtotime($worked_pause);
                 $differenceHours = abs(($endtimestamp - $starttimestamp)/3600);
                 $differenceMinutes = abs(($endtimestamp - $starttimestamp)/60);
                 if ($differenceMinutes % 60 != 0){
@@ -62,14 +66,31 @@ if (!empty($date) || !empty($start_time_hour) || !empty($end_time_hour)) {
                 } else {
                     $differenceMinutes = "00";
                 }
+                
+                if ($pausetimestamp != 0){
+                    $differenceTime = $date." ".$differenceHours.":".$differenceMinutes.":00";
+                    $differencetimestemp = strtotime($differenceTime);
+                    $differenceHours = abs(($differencetimestemp - $pausetimestamp)/3600);
+                    $differenceMinutes = abs(($differencetimestemp - $pausetimestamp)/60);
+                    if ($differenceMinutes % 60 != 0){
+                        $differenceHours = floor($differenceHours);
+                        if ($differenceMinutes % 60 <= 9) {
+                            $differenceMinutes = "0".($differenceMinutes % 60);
+                        } else {
+                            $differenceMinutes = ($differenceMinutes % 60);
+                        }
+                    } else {
+                        $differenceMinutes = "00";
+                    }
+                }
                 $difference = $differenceHours.":".$differenceMinutes.":00";
                 return $difference;
             }
             
-            $working_hours = differenceInHours($worked_start,$worked_end);
+            $working_hours = differenceInHours($worked_start,$worked_end, $worked_pause, $_POST['date']);
             
             $stmt=$connect->prepare($INSERT);
-            $stmt->bind_param("sissssss",$title, $p_nr, $date, $start_time, $pause_time, $end_time, $working_hours, $working_hours);
+            $stmt->bind_param("sissssss",$title, $p_nr, $date, $start_time, $pause_time, $end_time, $note, $working_hours);
             $stmt->execute();
             header('Location: http://zeiterfassung-wbh.de/Dashboard.html');
             exit;
